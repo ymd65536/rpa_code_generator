d35ac557f6923c5f808ed939184f6a33d6b0e9c8
Kento.Yamada <ymd65536@gmail.com>
2021-11-11 23:59:11 +0900

    ソースコード 生成スクリプト

---
+++ main.py
@@ -0,0 +1,27 @@
+import json
+
+def actions_define():
+    FILENAME = 'actions.json'
+    action_file = open(FILENAME,mode='r')
+    data = json.load(action_file)
+    action_file.close()
+    json_data = data['MainFunctions']
+    main_function_keys = json_data.keys()
+
+    for func in main_function_keys:
+        func_name = func
+        func_data = json_data[func]
+        if func_data['Type'] == 'Function':
+            print(func_data['In'] + " = " + func_name)
+            condition_value = func_data['Condition']['Value']
+            condition_type = func_data['Condition']['Type']
+
+            if condition_type == 'String':
+                condition_value = "\"" + condition_value + "\""
+
+            print("If " + func_name + "=" + condition_value + " Then")
+            print("End If")
+
+if __name__=='__main__':
+    # variable_define()
+    actions_define()
